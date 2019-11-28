03b2936100b39845823f9b9fdce8fb021db365e1
namyehyun <Nam.Yehyun.97@gmail.com>
2019-11-28 21:46:22 +0900

    Add graph.h graph.cpp

---
+++ srcs/graph.cpp
@@ -0,0 +1,58 @@
+#include "graph.h"
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+Graph::Graph(void): num_nodes(0), num_edges(0) {}
+
+Graph::Graph(int _num_nodes): num_nodes(_num_nodes), num_edges(0) {
+    assert(0 < num_nodes);
+    adj_matrix.resize(num_nodes, vector<bool>(num_nodes, false));
+}
+
+void Graph::init(int _num_nodes) {
+    assert(0 < _num_nodes);
+    num_nodes = _num_nodes;
+    adj_matrix.resize(num_nodes, vector<bool>(num_nodes, false));
+
+    return;
+}
+
+bool Graph::add_edge(int u, int v) {
+    assert(0 <= u && u < num_nodes);
+    assert(0 <= v && v < num_nodes);
+
+    if (adj_matrix[u][v] || adj_matrix[v][u]) {
+        return false;
+    }
+
+    adj_matrix[u][v] = adj_matrix[v][u] = true;
+    ++num_edges;
+
+    return true;
+}
+
+void Graph::print(void) {
+    cout << "Nodes: " << num_nodes << endl;
+    cout << "Edges: " << num_edges << endl;
+
+    cout << "Adjacency Matrix: " << endl;
+    cout << "  ";
+    for (int v = 0; v < num_nodes; ++v) {
+        cout << v % 10 << ' ';
+    }
+    cout << endl;
+    for (int u = 0; u < num_nodes; ++u) {
+        cout << u % 10 << ' ';
+        for (int v = 0; v < num_nodes; ++v) {
+            cout << adj_matrix[u][v] << ' ';
+        }
+        cout << endl;
+    }
+
+    return;
+}
+
---
+++ srcs/graph.h
@@ -0,0 +1,23 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <vector>
+
+using namespace std;
+
+class Graph {
+public:
+    int num_nodes;
+    int num_edges;
+    vector<vector<bool>> adj_matrix;
+
+    Graph(void);
+    Graph(int _num_nodes);
+
+    void init(int _num_nodes);
+    bool add_edge(int u, int v);
+    void print(void);
+};
+
+#endif
+
