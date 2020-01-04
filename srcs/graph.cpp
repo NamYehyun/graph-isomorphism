35f8db7469c77c45e89052e89ec896cc1f5d68c3
NamYehyun <Nam.Yehyun.97@gmail.com>
2020-01-05 01:52:17 +0900

    Implement refine()

--- srcs/graph.cpp
+++ srcs/graph.cpp
@@ -1,81 +1,171 @@
 #include "graph.h"
 
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-Node::Node(int _id): id(_id), colour(0) {}
+Node::Node(int _id): id(_id), tmp(0) {}
 
-Node::~Node(void) {}
+void Node::print(void) {
+	cout << id;
+}
+
+Cell::Cell(void): counted(false) {}
+
+void Cell::print(void) {
+	cout << "( ";
+	for (Node* node: nodes) {
+		cout << node->id << ' ';
+	}
+	cout << ")";
+}
+
+Partition::Partition(void): init(false), equitable(false) {}
+
+void Partition::print(void) {
+	for (Cell cell: cells) {
+		cell.print();
+		cout << ' ';
+	}
+}
 
 Graph::Graph(void): num_nodes(0), num_edges(0) {}
 
-Graph::Graph(int _num_nodes): num_nodes(_num_nodes), num_edges(0) {
-    assert(0 < num_nodes);
-
-    nodes.clear();
-    nodes.reserve(num_nodes);
-
-    id_to_node.clear();
-}
-
 Graph::~Graph(void) {
-    for (Node* node: nodes) {
-        delete node;
-    }
+	for (Node* node: nodes) {
+		delete node;
+	}
 }
 
 void Graph::init(int _num_nodes) {
-    assert(0 < _num_nodes);
-    num_nodes = _num_nodes;
-    
-    nodes.clear();
-    nodes.reserve(num_nodes);
+	assert(0 < _num_nodes);
+	num_nodes = _num_nodes;
 
-    id_to_node.clear();
+	id_to_node.clear();
 
-    return;
+	return;
 }
 
 void Graph::set_name(string _name) {
-    name = _name;
-    return;
+	name = _name;
+	return;
 }
 
 Node* Graph::get_node(int id) {
-    assert(0 <= id && id < num_nodes);
+	assert(0 <= id && id < num_nodes);
 
-    if (id_to_node.find(id) == id_to_node.end()) {
-        Node* node = new Node(id);
-        nodes.emplace_back(node);
-        id_to_node[id] = node;
-    }
+	if (id_to_node.find(id) == id_to_node.end()) {
+		Node* node = new Node(id);
+		nodes.insert(node);
+		id_to_node[id] = node;
+	}
 
-    return id_to_node[id];
+	return id_to_node[id];
 }
 
 void Graph::add_edge(int u, int v) {
-    assert(0 <= u && u < num_nodes);
-    assert(0 <= v && v < num_nodes);
+	assert(0 <= u && u < num_nodes);
+	assert(0 <= v && v < num_nodes);
 
-    Node* node_u = get_node(u);
-    Node* node_v = get_node(v);
+	Node* node_u = get_node(u);
+	Node* node_v = get_node(v);
 
-    node_u->neighbors.insert(node_v);
-    node_v->neighbors.insert(node_u);
+	adj_list[node_u].insert(node_v);
+	adj_list[node_v].insert(node_u);
 
-    ++num_edges;
+	++num_edges;
 
-    return;
+	return;
+}
+
+void Graph::refine(void) {
+	if (!pi.init) {
+		Cell cell;
+		for (Node* node: nodes) {
+			cell.nodes.emplace_back(node);
+		}
+
+		pi.cells.clear();
+		pi.cells.push_back(cell);
+
+		pi.init = true;
+	}
+
+	bool stable = true;
+
+	for (Node* node: nodes) {
+		node->tmp = 0;
+	}
+
+	queue<Cell*> uncounted_cells;
+	for (Cell& cell: pi.cells) {
+		cell.counted = false;
+		uncounted_cells.push(&cell);
+	}
+
+	do {
+		Cell* target = uncounted_cells.front(); uncounted_cells.pop();
+
+		for (Node* u: target->nodes) {
+			for (Node* v: nodes) {
+				if (adj_list[v].find(u) != adj_list[v].end()) {
+					++(v->tmp);
+				}
+			}
+		}
+
+		target->counted = true;
+
+		stable = true;
+		list<Cell>::iterator cell;
+		for (cell = pi.cells.begin(); cell != pi.cells.end(); ++cell) {
+			vector<Node*>& cnodes = cell->nodes;
+			sort(cnodes.begin(), cnodes.end(),
+					[](const Node* a, const Node* b) -> bool {
+					return a->tmp < b->tmp;
+					}
+				);
+
+			if (cnodes.front()->tmp != cnodes.back()->tmp) {
+
+				int idx = 0;
+				while (idx < static_cast<int>(cnodes.size())) {
+					Cell new_cell;
+					new_cell.nodes.emplace_back(cnodes[idx]);
+					while (idx < static_cast<int>(cnodes.size())-1 && cnodes[idx+1]->tmp == cnodes[idx]->tmp) {
+						new_cell.nodes.emplace_back(cnodes[idx+1]);
+						++idx;
+					}
+
+					pi.cells.insert(cell, new_cell);
+					uncounted_cells.push(&new_cell);
+					++idx;
+				}
+
+				cell = pi.cells.erase(cell);
+				if (cell == pi.cells.end()) {
+					break;
+				}
+
+				stable = false;
+			}
+		}
+	} while (!stable);
+
+	pi.equitable = true;
+	return;
 }
 
 void Graph::print(void) {
-    cout << "Graph `" << name << "`" << endl;
-    cout << "  # Nodes: " << num_nodes << endl;
-    cout << "  # Edges: " << num_edges << endl;
+	cout << "Graph `" << name << "`" << endl;
+	cout << "  # Nodes: " << num_nodes << endl;
+	cout << "  # Edges: " << num_edges << endl;
+	cout << "  pi: "; pi.print(); cout << endl;
 
-    return;
+	return;
 }
 
--- srcs/graph.h
+++ srcs/graph.h
@@ -1,6 +1,7 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
 
+#include <list>
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -10,33 +11,57 @@ using namespace std;
 
 class Node {
 public:
-    int id;
-    int colour;
-    unordered_set<Node*> neighbors;
+	int id;
+	int tmp;
 
-    Node(int _id);
-    ~Node(void);
+	Node(int _id);
+
+	void print(void);
+};
+
+class Cell {
+public:
+	bool counted;
+	vector<Node*> nodes;
+
+	Cell(void);
+
+	void print(void);
+};
+
+class Partition {
+public:
+	bool init;
+	bool equitable;
+	list<Cell> cells;
+
+	Partition(void);
+
+	void print(void);
 };
 
 class Graph {
 public:
-    string name;
-    int num_nodes;
-    int num_edges;
-    vector<Node*> nodes;
-    unordered_map<int, Node*> id_to_node;
+	string name;
+	int num_nodes;
+	int num_edges;
+	unordered_set<Node*> nodes;
+	unordered_map<int, Node*> id_to_node;
+	unordered_map<Node*, unordered_set<Node*>> adj_list;
 
-    Graph(void);
-    Graph(int _num_nodes);
-    ~Graph(void);
+	Partition pi;
 
-    void init(int _num_nodes);
-    void set_name(string _name);
+	Graph(void);
+	~Graph(void);
 
-    Node* get_node(int id);
-    void add_edge(int u, int v);
+	void init(int _num_nodes);
+	void set_name(string _name);
+	Node* get_node(int id);
+	void add_edge(int u, int v);
 
-    void print(void);
+	void refine(void);
+
+	void print(void);
 };
 
 #endif
